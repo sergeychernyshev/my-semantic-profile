d46782a52a888f58662bde8df3853a83c2c7e63c
sergey.chernyshev <sergey.chernyshev@caae0624-cc4e-0410-9ee5-cb2ab0f41848>
2008-09-15 21:10:19 +0000

    Tagging a first normal release

--- config.inc.php
+++ config.inc.php
@@ -1,21 +1,44 @@
 <?
+error_reporting(E_ALL);
+
 /**
  * Someconfiguration parameters
  */
-
-// change this to the full URL of your installation
-$baseURL = 'http://www.yoursite.com/profile/';
+$baseURL = 'http://www.sergeychernyshev.com/profile/';
 
 /**
  * location of Personal Profile Document
  */
-$profileDocument = $SPROOT.'/foaf.rdf';
-$profileDocumentURI = $baseURL.'foaf.rdf';
+$profileDocument = $SPROOT.'../sergey.rdf';
+$profileDocumentURI = '/sergey.rdf';
 
 /**
  * Uncomment for test cases
  */
-$profileDocument = $SPROOT.'/tests/timbl.rdf';
-$profileDocumentURI = $baseURL.'tests/timbl.rdf';
+if (strpos($_SERVER['REQUEST_URI'], '/timbl/') !== false )
+{
+	$profileDocument = $SPROOT.'tests/timbl.rdf';
+	$profileDocumentURI = $baseURL.'tests/timbl.rdf';
+}
+if (strpos($_SERVER['REQUEST_URI'], '/cygri/') !== false )
+{
+	$profileDocument = $SPROOT.'tests/cygri.rdf';
+	$profileDocumentURI = $baseURL.'tests/cygri.rdf';
+}
+if (strpos($_SERVER['REQUEST_URI'], '/tomheath/') !== false )
+{
+	$profileDocument = $SPROOT.'tests/tomheath.rdf';
+	$profileDocumentURI = $baseURL.'tests/tomheath.rdf';
+}
+if (strpos($_SERVER['REQUEST_URI'], '/marco/') !== false )
+{
+	$profileDocument = $SPROOT.'tests/marco.rdf';
+	$profileDocumentURI = $baseURL.'tests/marco.rdf';
+}
+if (strpos($_SERVER['REQUEST_URI'], '/danbri/') !== false )
+{
+	$profileDocument = $SPROOT.'tests/danbri.rdf';
+	$profileDocumentURI = $baseURL.'tests/danbri.rdf';
+}
 
-$googleMapsKey = '-- insert Google Maps API key here --';
+$googleMapsKey = 'ABQIAAAAq_i4aTseMGLic8bgu1NQHRSEs_qikIHa8VCb2-5R0mAlXQZKPRRLkUrkPAVXUBMadjFafv4_Xrmr0g';
