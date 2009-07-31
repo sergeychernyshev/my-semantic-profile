7757c25017348f7874e8067e608498a6541a2d81
sergey.chernyshev <sergey.chernyshev@caae0624-cc4e-0410-9ee5-cb2ab0f41848>
2009-07-31 19:43:39 +0000

    Moved triples to be last in the tabs.

--- admin/modules/base.inc.php
+++ admin/modules/base.inc.php
@@ -23,6 +23,6 @@ include_once('basic/index.php');
 #include_once('pictures/index.php');
 include_once('people/index.php');
 include_once('location/index.php');
-include_once('triples/index.php');
 include_once('accounts/index.php');
 include_once('interests/index.php');
+include_once('triples/index.php');
